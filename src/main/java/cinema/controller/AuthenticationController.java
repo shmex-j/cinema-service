997590cc471373735f734fe1012e239f62ffaa9b
Maksym Shveda <mshveda28@gmail.com>
2022-12-15 20:57:46 +0200

    Fix setting role on register

--- src/main/java/cinema/controller/AuthenticationController.java
+++ src/main/java/cinema/controller/AuthenticationController.java
@@ -2,12 +2,10 @@ package cinema.controller;
 
 import cinema.dto.request.UserRequestDto;
 import cinema.dto.response.UserResponseDto;
-import cinema.model.Role;
 import cinema.model.User;
 import cinema.service.AuthenticationService;
 import cinema.service.RoleService;
 import cinema.service.mapper.ResponseDtoMapper;
-import java.util.Set;
 import javax.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthenticationController {
-    private static final Role.RoleName DEFAULT_ROLE = Role.RoleName.USER;
     private final AuthenticationService authService;
     private final RoleService roleService;
     private final ResponseDtoMapper<UserResponseDto, User> userDtoResponseMapper;
@@ -32,8 +29,6 @@ public class AuthenticationController {
     @PostMapping("/register")
     public UserResponseDto register(@RequestBody @Valid UserRequestDto requestDto) {
         User user = authService.register(requestDto.getEmail(), requestDto.getPassword());
-        Role role = roleService.getByName(DEFAULT_ROLE.name());
-        user.setRoles(Set.of(role));
         return userDtoResponseMapper.mapToDto(user);
     }
 }
--- src/main/java/cinema/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/cinema/service/impl/AuthenticationServiceImpl.java
@@ -1,19 +1,26 @@
 package cinema.service.impl;
 
+import cinema.model.Role;
 import cinema.model.User;
 import cinema.service.AuthenticationService;
+import cinema.service.RoleService;
 import cinema.service.ShoppingCartService;
 import cinema.service.UserService;
+import java.util.Set;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
+    private static final Role.RoleName DEFAULT_ROLE = Role.RoleName.USER;
     private final UserService userService;
+    private final RoleService roleService;
     private final ShoppingCartService shoppingCartService;
 
     public AuthenticationServiceImpl(UserService userService,
+                                     RoleService roleService,
                                      ShoppingCartService shoppingCartService) {
         this.userService = userService;
+        this.roleService = roleService;
         this.shoppingCartService = shoppingCartService;
     }
 
@@ -22,6 +29,8 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         User user = new User();
         user.setEmail(email);
         user.setPassword(password);
+        Role role = roleService.getByName(DEFAULT_ROLE.name());
+        user.setRoles(Set.of(role));
         userService.add(user);
         shoppingCartService.registerNewShoppingCart(user);
         return user;
